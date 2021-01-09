8b4953591c38576ced1cb16853e3f61563cabb44
Peter Turczak <peter@turczak.de>
2021-01-09 11:14:49 +0100

    Add comments and normalization

--- samples/convert_dir.sh
+++ samples/convert_dir.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Prepare sound samples to be written to SD card,
+# downsample and reduce bit depth.
 mkdir -p ../sd
 cd $1
-find . -name '*wav' -exec sox {} -r 16000 -b 8 ../../sd/{} \;
+find . -name '*wav' -exec sox --norm=-3 {} -r 16000 -b 8 ../../sd/{} \;
